08ebce5151c20560a50278412a1e4ca5f9fe6b62
chandu89 <chandanwtb2009@gmail.com>
2024-03-19 16:53:20 +0100

    Removed third party gem to consume URI

--- app/controllers/api/v1/repositories_controller.rb
+++ app/controllers/api/v1/repositories_controller.rb
@@ -4,28 +4,27 @@ class Api::V1::RepositoriesController < ApplicationController
     sort_by = params[:sort_by].presence || 'stars' # default sort by stars
     per_page = params[:per_page].presence || 10
     page = params[:page].presence || 1
-    order = params[:order].presence || "desc"
+    order = params[:order].presence || 'desc' # default order is descending
+    
+    uri = URI("#{GIT_REPO_URL}?q=#{search_term}&sort=#{sort_by}&order=#{order}&per_page=#{per_page}&page=#{page}")
+    response = Net::HTTP.get(uri)
+    repositories = parse_response(response)
 
-    response = HTTParty.get("#{GIT_REPO_URL}?q=#{search_term}&sort=#{sort_by}&order=#{order}&per_page=#{per_page}&page=#{page}")
-    if response.success?
-      render json: format_response(response), status: :ok
-    else
-      render json: { error: 'Failed to fetch repositories' }, status: :unprocessable_entity
-    end
+    render json: repositories
   end
 
   private
 
-  def format_response(response)
-    repositories = response['items'].map do |repo|
+  def parse_response(response)
+    parsed_response = JSON.parse(response)
+    repositories = parsed_response['items'].map do |item|
       {
-        name: repo['name'],
-        author: repo['owner']['login'],
-        repository_url: repo['html_url'],
-        stars: repo['stargazers_count']
+        name: item['name'],
+        author: item['owner']['login'],
+        repository_url: item['html_url'],
+        stars: item['stargazers_count']
       }
     end
-
-    { repositories: repositories }
+    repositories
   end
 end
--- spec/controllers/api/v1/repositories_controller_spec.rb
+++ spec/controllers/api/v1/repositories_controller_spec.rb
@@ -6,22 +6,24 @@ RSpec.describe Api::V1::RepositoriesController, type: :controller, vcr: { casset
     let(:search_term) { 'git_search' }
     let(:per_page) { 10 }
     let(:page) { 1 }
+  
     context 'when the request is successful' do
       before do
-        get :index, params: { search_term: search_term, per_page: per_page, page: page }
+        get :index, params: { search_term: search_term, per_page: per_page, page: page}
       end
+      
       it 'returns a successful response' do
         expect(response).to have_http_status(:ok)
       end
 
-      it 'returns JSON with repositories' do
-        json_response = JSON.parse(response.body)
-        expect(json_response['repositories']).not_to be_empty
-      end
-
-      it 'returns formatted JSON with repositories' do
-        json_response = JSON.parse(response.body)
-        expect(json_response['repositories']).not_to be_empty
+      context "when there is default sorting order" do
+        it 'returns formatted JSON in stars desc order' do
+          json_response = JSON.parse(response.body)
+          expect(json_response).not_to be_empty
+          stars = json_response.collect{|j| j['stars']}
+          is_desc = stars.each_cons(2).all? { |a, b| a >= b }
+          expect(is_desc).to be_truthy
+        end
       end
     end
   end
