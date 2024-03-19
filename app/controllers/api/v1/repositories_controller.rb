d9aac4975e6520393dcd733e4f77c71ce8546dcb
chandu89 <chandanwtb2009@gmail.com>
2024-03-19 13:15:04 +0100

    Added REPO_URL constant, added code for tests

--- app/controllers/api/v1/repositories_controller.rb
+++ app/controllers/api/v1/repositories_controller.rb
@@ -1,4 +1,16 @@
 class Api::V1::RepositoriesController < ApplicationController
   def index
+    search_term = params[:search_term]
+    sort_by = params[:sort_by].presence || 'stars' # default sort by stars
+    per_page =1 
+    page = params[:page].presence || 1
+    
+    response = HTTParty.get("#{GIT_REPO_URL}?q=#{search_term}&sort=#{sort_by}&per_page=#{per_page}&page=#{page}")
+
+    if response.success?
+      render json: response.body, status: :ok
+    else
+      render json: { error: 'Failed to fetch repositories' }, status: :unprocessable_entity
+    end
   end
 end
--- spec/controllers/api/v1/repositories_controller_spec.rb
+++ spec/controllers/api/v1/repositories_controller_spec.rb
@@ -6,24 +6,26 @@ RSpec.describe Api::V1::RepositoriesController, type: :controller do
     let(:search_term) { 'rails' }
     let(:per_page) { 10 }
     let(:page) { 1 }
-
-    before do
-      allow(HTTParty).to receive(:get).and_return(success_response)
-    end
-
     context 'when the request is successful' do
       let(:success_response) do
-        {}
+        {
+          'items' => [
+            { 'name' => 'git_search', 'owner' => { 'login' => 'chandan' }, 'html_url' => 'https://github.com/rails/rails', 'stargazers_count' => 100 }
+          ]
+        }
       end
-
-      it 'returns a successful response' do
+      before do
+        allow(HTTParty).to receive(:get).and_return(double(success?: true, body: success_response.to_json))
         get :index, params: { search_term: search_term, per_page: per_page, page: page }
+      end
+      it 'returns a successful response' do
         expect(response).to have_http_status(:ok)
       end
 
       it 'returns JSON with repositories' do
         get :index, params: { search_term: search_term, per_page: per_page, page: page }
-        expect(response['repositories']).not_to be_empty
+        json_response = JSON.parse(response.body)
+        expect(json_response['items']).not_to be_empty
       end
     end
   end
